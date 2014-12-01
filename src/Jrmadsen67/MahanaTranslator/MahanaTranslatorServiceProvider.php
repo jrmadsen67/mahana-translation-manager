2aca3f713fb4206ca3c9d5dc900eb19f413163ab
Jeff Madsen <jrmadsen67@gmail.com>
2014-12-01 12:58:42 +0900

    initial commit

---
+++ src/Jrmadsen67/MahanaTranslator/MahanaTranslatorServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Jrmadsen67\MahanaTranslator;
+
+use Illuminate\Support\ServiceProvider;
+
+class MahanaTranslatorServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('jrmadsen67/mahana-translator');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
