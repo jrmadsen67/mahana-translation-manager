de5502d756a2b75b1486b672f40416a5cc79b060
Jeff Madsen <jrmadsen67@gmail.com>
2014-12-01 14:25:01 +0900

    repo methods

--- src/Jrmadsen67/MahanaTranslationManager/repositories/TranslationManagerEloquentRepository.php
+++ src/Jrmadsen67/MahanaTranslationManager/repositories/TranslationManagerEloquentRepository.php
@@ -7,7 +7,35 @@ use Config;
 class TranslationManagerEloquentRepository implements TranslationManagerRepositoryInterface
 {
 
+	public function getValue($key, $lang_code)
+	{
+		return Translations::whereKey($key)->whereLangCode($lang_code)->first();
+	}
+	public function getLanguageSet($lang_code)
+	{
+		return Translations::whereLangCode($lang_code)->get();
+	}
+	public function getNeedTranslation($lang_code)
+	{
 
+	}
+	public function getNeedUpdate($lang_code)
+	{
+		return Translations::whereLangCode($lang_code)->whereRequireUpdate(1)->get();
+	}
+	public function getNeedManualtranslation($lang_code)
+	{
+		return Translations::whereLangCode($lang_code)->whereRequireManualTranslation(1)->get();
+	}
+	public function updateValue($key, $lang_code, $data, $cascade = true)
+	{
+		$translation = self::getValue($key, $lang_code);
+		$translation->fill($data);
+		$translation->save();
+
+		return Translations::whereLangCode($lang_code)->update(array('requires_update' => 1));
+
+	} 
 
 
 
--- src/Jrmadsen67/MahanaTranslationManager/repositories/TranslationManagerRepositoryInterface.php
+++ src/Jrmadsen67/MahanaTranslationManager/repositories/TranslationManagerRepositoryInterface.php
@@ -4,5 +4,10 @@
  */
 interface TranslationManagerRepositoryInterface
 {
-
+	public function getValue($key, $lang_code);
+	public function getLanguageSet($lang_code);
+	public function getNeedTranslation($lang_code);
+	public function getNeedUpdate($lang_code);
+	public function getNeedManualtranslation($lang_code);
+	public function updateValue($key, $lang_code, $data, $cascade); 
 }
